607eee4dfd70be7067b380d9b7f8aa0db713ae37
matm89 <55142684+matm89@users.noreply.github.com>
2025-12-14 19:07:03 +0100

    feat(homepage-signup): added the forms element in both pages
    added all the element in both elements

--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,13 @@ export default function HomePage() {
       <Navbar isAuthenticated={false}/>
       <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
         <main className='flex min-h-screen w-full flex-col items-center justify-between bg-white dark:bg-black sm:items-start'>
-          <div className='flex min-h-svh w-full items-center justify-center p-4 md:p-10'>
+          <div className='flex min-h-svh w-full items-center justify-center md:p-1'>
             <div className='w-full max-w-sm'>
-              <div className='flex items-center flex-col gap-4'>
-                <div className='flex min-h-svh w-full items-center justify-center p-6 md:p-10'>
+              <div className='flex items-center flex-col gap-2 md:p-1'>
                   <div className='w-full max-w-sm'>
                     <LoginForm />
                   </div>
-                </div>
-                <SignInButtonGithub /> 
+                  <SignInButtonGithub /> 
               </div>
             </div>
           </div>
