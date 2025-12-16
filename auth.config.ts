88f664785b96bc5af11dbd0632411012fe9f473a
matm89 <55142684+matm89@users.noreply.github.com>
2025-12-16 15:01:58 +0100

    fix(routing): fixed wrong routes
    added the proper routes to /

--- auth.config.ts
+++ auth.config.ts
@@ -7,14 +7,14 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      const isOnDashboard = nextUrl.pathname.startsWith('/');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
         // Redirect to dashboard if already logged in and on home page
         if (nextUrl.pathname === '/') {
-          return Response.redirect(new URL('/dashboard', nextUrl));
+          return Response.redirect(new URL('/', nextUrl));
         }
       }
       return true;
